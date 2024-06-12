53d65cff537c1769dadf52f2f3a3710b1608d5a3
Byron2016 <bgva2005@yahoo.com>
2024-06-11 19:45:14 -0500

    feat: first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+<div>
+	<div>
+		<img src=https://raw.githubusercontent.com/Byron2016/00_forImages/main/images/Logo_01_00.png align=left alt=MyLogo width=200>
+	</div>
+	&nbsp;
+	<div>
+		<h1>081_CSharp_Distrib</h1>
+	</div>
+</div>
+
+&nbsp;
+
+# Table of contents
+
+---
+
+- [Table of contents](#table-of-contents)
+- [Project Description](#project-description)
+- [Technologies used](#technologies-used)
+- [References](#references)
+- [Steps](#steps)
+
+[⏪(Back to top)](#table-of-contents)
+
+# Project Description
+
+---
+
+[⏪(Back to top)](#table-of-contents)
+&nbsp;
+
+# Technologies used
+
+---
+
+![C#](https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=csharp&logoColor=white)
+![VisualStudio](https://img.shields.io/badge/Visual%20Studio-5C2D91.svg?style=for-the-badge&logo=visual-studio&logoColor=white)
+
+[⏪(Back to top)](#table-of-contents)
+
+# References
+
+---
+
+- Shields.io
+
+  - [Shields.io](https://shields.io/)
+
+  - [Github Ileriayo markdown-badges](https://github.com/Ileriayo/markdown-badges)
+
+  - [Github Ileriayo markdown-badges WebSite](https://ileriayo.github.io/markdown-badges/)
+
+[⏪(Back to top)](#table-of-contents)
+
+# Steps
+
+
+---
+
+[⏪(Back to top)](#table-of-contents)
---
+++ script.sh
@@ -0,0 +1,92 @@
+#! /bin/bash
+# To run: bash script.sh  
+echo "<div>" >> README.md
+echo "	<div>" >> README.md
+echo "		<img src="https://raw.githubusercontent.com/Byron2016/00_forImages/main/images/Logo_01_00.png" align="left" alt="MyLogo" width="200">" >> README.md
+echo "	</div>" >> README.md
+echo "	&nbsp;" >> README.md
+echo "	<div>" >> README.md
+echo "		<h1>081_CSharp_Distrib</h1>" >> README.md
+echo "	</div>" >> README.md
+echo "</div>" >> README.md
+echo "" >> README.md
+echo "&nbsp;" >> README.md
+
+echo "" >> README.md
+echo "# Table of contents" >> README.md
+echo "" >> README.md
+echo "--- " >> README.md
+echo "" >> README.md
+echo "- [Table of contents](#table-of-contents)" >> README.md
+echo "- [Project Description](#project-description)" >> README.md
+echo "- [Technologies used](#technologies-used)" >> README.md
+echo "- [References](#references)" >> README.md
+echo "- [Steps](#steps)" >> README.md
+echo "" >> README.md
+echo "  - <details> <summary>List of Steps</summary>" >> README.md
+echo "" >> README.md
+echo "    - [Install & Setup Vite + React + Bootstrap 5](#-artificial-intelligence-and-bots)" >> README.md
+echo "" >> README.md
+echo "   </details>" >> README.md
+echo "" >> README.md
+echo "[⏪(Back to top)](#table-of-contents)" >> README.md
+
+
+echo "" >> README.md
+echo "# Project Description" >> README.md
+echo "" >> README.md
+echo "--- " >> README.md
+echo "" >> README.md
+echo "[⏪(Back to top)](#table-of-contents)" >> README.md
+echo "&nbsp;" >> README.md
+
+
+echo "" >> README.md
+echo "# Technologies used" >> README.md
+echo "" >> README.md
+echo "--- " >> README.md
+echo "" >> README.md
+echo "![C#](https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=csharp&logoColor=white)" >> README.md
+echo "" >> README.md
+echo "![VisualStudio](https://img.shields.io/badge/Visual%20Studio-5C2D91.svg?style=for-the-badge&logo=visual-studio&logoColor=white)" >> README.md
+echo "" >> README.md
+echo "[⏪(Back to top)](#table-of-contents)" >> README.md
+
+
+echo "" >> README.md
+echo "# References" >> README.md
+echo "" >> README.md
+echo "--- " >> README.md
+echo "" >> README.md
+echo "- Shields.io" >> README.md
+echo "" >> README.md
+echo "  - [Shields.io](https://shields.io/)" >> README.md
+echo "" >> README.md
+echo "  - [Github Ileriayo markdown-badges](https://github.com/Ileriayo/markdown-badges)" >> README.md
+echo "" >> README.md
+echo "  - [Github Ileriayo markdown-badges WebSite](https://ileriayo.github.io/markdown-badges/)" >> README.md
+echo "" >> README.md
+echo "[⏪(Back to top)](#table-of-contents)" >> README.md
+
+
+echo "" >> README.md
+echo "# Steps" >> README.md
+echo "" >> README.md
+echo "--- " >> README.md
+echo "" >> README.md
+echo "  " >> README.md
+echo "" >> README.md
+echo "[⏪(Back to top)](#table-of-contents)" >> README.md
+
+# upload to github 
+
+# echo "# 081_CSharp_Distrib" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/Byron2016/081_CSharp_Distrib.git
+# git push -u origin main
+
+
+
